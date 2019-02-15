456942f0757a1608886cd41b2cf8a0d4511efde4
Auria Aharon <auria@storemaven.com>
2019-02-15 21:02:13 +0200

    First commit, added REST API to access items, stores, and user authentication.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Welcome To Restfull API with Flask Example
+
+## Installing
+
+```
+pip install flask
+```
+
---
+++ resources/store.py
@@ -0,0 +1,34 @@
+from flask_restful import Resource
+
+from models.store import StoreModel
+
+
+class Store(Resource):
+
+    def get(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            return store.json()
+
+        return {'message': 'Store not found'}, 404
+
+    def post(self, name):
+        if StoreModel.find_by_name(name):
+            return {'message': 'Store already exists'}, 400
+        store = StoreModel(name)
+        store.save_to_db()
+
+        return {'message': 'Created new store'}, 201
+
+    def delete(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            store.delete_from_db()
+
+        return {'message': 'Store has been deleted'}
+
+
+class StoreList(Resource):
+
+    def get(self):
+        return {'stores': [store for store in StoreModel.get_all()]}
