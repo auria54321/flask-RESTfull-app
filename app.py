b43a1fa65ed743a3007b0adff23a8fd48c6ccb09
Auria Aharon <auria@storemaven.com>
2019-02-16 12:31:22 +0200

    Fixed db not found error

--- app.py
+++ app.py
@@ -46,13 +46,6 @@ api.add_resource(Store, '/store/<name>')
 api.add_resource(StoreList, '/stores')
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
     app.run(port=5050, debug=True)
 
